1944b669a99fe02281c55c7fbfbdc20f666e2b36
marcosffn <marcos_felipefolhas@hotmail.com>
2021-01-05 11:34:03 -0300

    Update
    Atualização de formatação.

--- Cálculo Trabalhistas.py
+++ Cálculo Trabalhistas.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 salario = float(input("Valor do salário: R$ ").replace(",", "."))
 
 horas = 220
@@ -137,7 +138,7 @@ if trabalhou_extra == "s":
 else:
     pass
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 quanto3 = 0.
 valor_hora_extra4 = 0.
 
@@ -173,12 +174,12 @@ if trabalhou_extra == "s":
         pass
 else:
     pass
-# ! # # # # # # # # Cálculo do DSR # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# ! # # # # # # # # Cálculo do DSR # # # # # # # # # # # # # # # # # # # # # #
 dsr = 0.
 dsr1 = 0.
 dsr2 = 0.
 dsr3 = 0.
-## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 falta_dias = 0
 falta_horas = 0.
 falta_valor = 0.
@@ -190,7 +191,6 @@ if faltou == "s":
     falta_dias = int(input("Faltou quantos dias? "))
     falta_horas = float(input("Faltou por quantas horas? "))
     falta_valor = salario / horas * falta_horas
-    falta_valor_dsr = salario / horas * (horas/5/5.5)
 else:
     pass
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -211,13 +211,13 @@ if valor_hora_extra2 != 0:
 else:
     pass
 if valor_hora_extra3 != 0:
-    dsr2 = (quanto2/dias_uteis) * \
-        (valor_hora_extra3/quanto2) * domingos_feriados
+    dsr2 = (quanto2 / dias_uteis) * \
+        (valor_hora_extra3 / quanto2) * domingos_feriados
 else:
     pass
 if valor_hora_extra4 != 0:
-    dsr3 = (quanto3/dias_uteis) * \
-        (valor_hora_extra4/quanto2) * domingos_feriados
+    dsr3 = (quanto3 / dias_uteis) * \
+        (valor_hora_extra4 / quanto2) * domingos_feriados
 else:
     pass
 
@@ -225,7 +225,10 @@ else:
 
 total_proventos = salario + periculosidade + \
     insalubridade + dsr + dsr1 + dsr2 + dsr3 + valor_hora_extra1 + \
-    valor_hora_extra2 + valor_hora_extra3 - (falta_valor + falta_valor_dsr)
+    valor_hora_extra2 + valor_hora_extra3
+
+total_proventos_menos_faltas = total_proventos - \
+    (falta_valor + falta_valor_dsr)
 
 # # # # # # # # # # # # # # # # # # # # INSS # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 aliquota1 = 0.075
@@ -240,43 +243,40 @@ limite4 = 6101.06
 inss = 0.
 
 faixa1 = limite1 * aliquota1
-faixa2 = (limite2 - limite1) * aliquota2
-faixa3 = (limite3 - limite2) * aliquota3
-faixa4 = (limite4 - limite3) * aliquota4
+faixa2 = (limite2 - (limite1 + 0.01)) * aliquota2
+faixa3 = (limite3 - (limite2 + 0.01)) * aliquota3
+faixa4 = (limite4 - (limite3 + 0.1)) * aliquota4
 
 valor_residual = 0.
 
 if salario <= limite1:
     valor_residual = 0.
-    inss = salario * aliquota1
+    inss = total_proventos_menos_faltas * aliquota1
 
-elif salario <= limite2 and salario > limite1:
+elif salario <= limite2 and total_proventos_menos_faltas > limite1:
     valor_residual = 0.
-    valor_residual = (salario - limite1) * aliquota2
+    valor_residual = (total_proventos_menos_faltas - limite1) * aliquota2
     inss = faixa1 + valor_residual
 
-elif salario <= limite3 and salario > limite2:
+elif salario <= limite3 and total_proventos_menos_faltas > limite2:
     valor_residual = 0.
-    valor_residual = (salario - limite2) * aliquota3
+    valor_residual = (total_proventos_menos_faltas - limite2) * aliquota3
     inss = valor_residual + faixa1 + faixa2
 
-elif salario <= limite4 and salario > limite3:
+elif salario <= limite4 and total_proventos_menos_faltas > limite3:
     valor_residual = 0.
-    valor_residual = (salario - limite3) * aliquota4
+    valor_residual = (total_proventos_menos_faltas - limite3) * aliquota4
     inss = faixa1 + faixa2 + faixa3 + valor_residual
 else:
     pass
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-    # * # # # # # # # # # # # Interface # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# * # # # # # # # # # # # Interface # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 print("\n")
 print(f"Salário: R$ {salario:.2f}")
 print(f"Valor da hora: R$ {valor_hora:.2f}")
 print(f"Valor da hora noturna: R$ {valor_hora_noturna:.2f}")
-print(f"Valor das horas faltadas: R$ {falta_valor:.2f}")
-print(f"Valor da DSR das horas faltadas: R$ {falta_valor_dsr:.2f}")
-print(
-    f"Total de desconto por ter faltado: R$ {falta_valor+falta_valor_dsr:.2f}")
-print(f"Valor do desconto do INSS: R$ {inss:.2f}")
 print(f"Insalubridade: R$ {insalubridade:.2f}")
 print(f"Periculosidade: R$ {periculosidade:.2f}")
 print(f"Valor das horas extras 1: R$ {valor_hora_extra1:.2f}")
@@ -284,7 +284,7 @@ print(f"Valor das horas extras 2: R$ {valor_hora_extra2:.2f}")
 print(f"Valor das horas extras 3: R$ {valor_hora_extra3:.2f}")
 print(f"Valor das horas extras 4: R$ {valor_hora_extra4:.2f}")
 print(
-    f"Valor hora extra 1, 2, 3 e 4: R$ {(valor_hora_extra1+valor_hora_extra2+valor_hora_extra3+valor_hora_extra4):.2f}")
+    f"Valor hora extra 1, 2, 3 e 4: R$ {(valor_hora_extra1 + valor_hora_extra2 + valor_hora_extra3 + valor_hora_extra4): .2f}")
 print(f"Descanso semanal remunerado 1: R$ {dsr:.2f}")
 print(f"Descanso semanal remunerado 2: R$ {dsr1:.2f}")
 print(f"Descanso semanal remunerado 3: R$ {dsr2:.2f}")
@@ -292,3 +292,10 @@ print(f"Descanso semanal remunerado 4: R$ {dsr3:.2f}")
 print(
     f"Descanso semanal remunerado 1, 2, 3 e 4: R$ {(dsr+dsr1+dsr2+dsr3):.2f}")
 print(f"Total de proventos: R$ {total_proventos:.2f}")
+print(f"Valor das horas faltadas: R$ {falta_valor:.2f}")
+print(f"Valor da DSR das horas faltadas: R$ {falta_valor_dsr:.2f}")
+print(
+    f"Total de desconto por ter faltado: R$ {falta_valor+falta_valor_dsr:.2f}")
+print(f"Salário pré-líquido: R$ {total_proventos_menos_faltas:.2f}")
+print(f"Valor do desconto do INSS: R$ {inss:.2f}")
+print(f"Salário líquido: R$ {total_proventos_menos_faltas - inss:.2f}")
--- Cálculos Trabalhistas 2.py
+++ Cálculos Trabalhistas 2.py
@@ -11,7 +11,9 @@ def adicionais(sal, tipo=("p", "i1", "i2", "i3")):
     return adicional
 
 
-def hora_extra(quantas, valor_hora, valor_horanoturna, qual=("0", "1", "2", "3", "4")):
+def hora_extra(
+    quantas, valor_hora, valor_horanoturna, qual=("0", "1", "2", "3", "4")
+):
     """
     Horas extras normais[0], hora extra noturna[1], hora notura[2],
     hora extra domingo e feriados[3], hora extra domingo e feriados noturno[4]
@@ -31,45 +33,63 @@ def hora_extra(quantas, valor_hora, valor_horanoturna, qual=("0", "1", "2", "3",
         valor_hora = (valor_hora_noturna * 2) * 1.2 * quantas
     return valor_hora
 
+
 def faltas(horas, salario_bruto, dias_q_faltou, carga_horaria):
-    preco_falta = (salario_bruto/220) * horas
-    
+    preco_falta = (salario_bruto / 220) * horas
+
     if dias_q_faltou != 0:
-            falta_dsr = (salario_bruto/220) * (carga_horaria * dias_q_faltou)
+        falta_dsr = (salario_bruto / 220) * (carga_horaria * dias_q_faltou)
     else:
-        falta_dsr = 0.
-    
+        falta_dsr = 0.0
+
     total = preco_falta + falta_dsr
 
     return total
 
 
-def dsr(dia_uteis, domingos_feriado, quantidade_de_horas_extras=(), horas_extras=()):
-    dsrs1 = 0.
-    dsrs2 = 0.
-    dsrs3 = 0.
-    dsrs4 = 0.
-    dsrs5 = 0.
+def dsr(
+    dia_uteis, domingos_feriado, quantidade_de_horas_extras=(), horas_extras=()
+):
+    dsrs1 = 0.0
+    dsrs2 = 0.0
+    dsrs3 = 0.0
+    dsrs4 = 0.0
+    dsrs5 = 0.0
 
     if horas_extra_1 != 0:
-        dsrs1 = (quantidade_de_horas_extras[0] / dia_uteis) * \
-            (horas_extras[0] / quantidade_de_horas_extras[0]) * domingos_feriado
+        dsrs1 = (
+            (quantidade_de_horas_extras[0] / dia_uteis)
+            * (horas_extras[0] / quantidade_de_horas_extras[0])
+            * domingos_feriado
+        )
 
     if horas_extra_2 != 0:
-        dsrs2 = (quantidade_de_horas_extras[1] / dia_uteis) * \
-            (horas_extras[1] / quantidade_de_horas_extras[1]) * domingos_feriado
+        dsrs2 = (
+            (quantidade_de_horas_extras[1] / dia_uteis)
+            * (horas_extras[1] / quantidade_de_horas_extras[1])
+            * domingos_feriado
+        )
 
     if horas_extra_3 != 0:
-        dsrs3 = (quantidade_de_horas_extras[2] / dia_uteis) * \
-            (horas_extras[2] / quantidade_de_horas_extras[2]) * domingos_feriado
+        dsrs3 = (
+            (quantidade_de_horas_extras[2] / dia_uteis)
+            * (horas_extras[2] / quantidade_de_horas_extras[2])
+            * domingos_feriado
+        )
 
     if horas_extra_4 != 0:
-        dsrs4 = (quantidade_de_horas_extras[3] / dia_uteis) * \
-            (horas_extras[3] / quantidade_de_horas_extras[3]) * domingos_feriado
+        dsrs4 = (
+            (quantidade_de_horas_extras[3] / dia_uteis)
+            * (horas_extras[3] / quantidade_de_horas_extras[3])
+            * domingos_feriado
+        )
 
     if horas_extra_5 != 0:
-        dsrs5 = (quantidade_de_horas_extras[4] / dia_uteis) * \
-            (horas_extras[4] / quantidade_de_horas_extras[4]) * domingos_feriado
+        dsrs5 = (
+            (quantidade_de_horas_extras[4] / dia_uteis)
+            * (horas_extras[4] / quantidade_de_horas_extras[4])
+            * domingos_feriado
+        )
 
     dsrs = [dsrs1, dsrs2, dsrs3, dsrs4, dsrs5]
     return dsrs
@@ -85,30 +105,30 @@ def inss(salario):
     limite2 = 2089.60
     limite3 = 3134.40
     limite4 = 6101.06
-    inss = 0.
+    inss = 0.0
 
     faixa1 = limite1 * aliquota1
     faixa2 = (limite2 - (limite1 + 0.01)) * aliquota2
     faixa3 = (limite3 - (limite2 + 0.01)) * aliquota3
 
-    valor_residual = 0.
+    valor_residual = 0.0
 
     if salario <= limite1:
-        valor_residual = 0.
+        valor_residual = 0.0
         inss = total_proventos_menos_faltas * aliquota1
 
     elif salario <= limite2 and total_proventos_menos_faltas > limite1:
-        valor_residual = 0.
+        valor_residual = 0.0
         valor_residual = (total_proventos_menos_faltas - limite1) * aliquota2
         inss = faixa1 + valor_residual
 
     elif salario <= limite3 and total_proventos_menos_faltas > limite2:
-        valor_residual = 0.
+        valor_residual = 0.0
         valor_residual = (total_proventos_menos_faltas - limite2) * aliquota3
         inss = valor_residual + faixa1 + faixa2
 
     elif salario <= limite4 and total_proventos_menos_faltas > limite3:
-        valor_residual = 0.
+        valor_residual = 0.0
         valor_residual = (total_proventos_menos_faltas - limite3) * aliquota4
         inss = faixa1 + faixa2 + faixa3 + valor_residual
     elif salario > 6101.06:
@@ -117,13 +137,13 @@ def inss(salario):
 
 
 def irrf(valor, dependentes, pensao, previdencia):
-    
+
     deducao = 189.59 * dependentes
-    
+
     valor = valor - deducao - pensao - previdencia
-    
+
     if valor <= 1903.98:
-        irrf = 0.
+        irrf = 0.0
     elif valor <= 2826.65 and valor > 1903.99:
         irrf = valor * 0.075 - 142.8
     elif valor <= 3751.05 and valor > 2826.66:
@@ -132,17 +152,16 @@ def irrf(valor, dependentes, pensao, previdencia):
         irrf = valor * 0.225 - 636.13
     elif valor > 4664.68:
         irrf = valor * 0.275 - 869.36
-    
-    
-        
-        
+
     return irrf
 
 
 print("Digite o valor do salário: ")
-salario = float(input('> '))
+salario = float(input("> "))
 
-print("Digite o valor da carga horária diária, para contar o desconto em faltas: ")
+print(
+    "Digite o valor da carga horária diária, para contar o desconto em faltas: "
+)
 carga = float(input("> "))
 
 print("Qual adicional ele recebe: ")
@@ -155,53 +174,74 @@ salario_adicional = salario + adicional_valor
 valor_da_hora = salario_adicional / 220
 valor_hora_noturna = valor_da_hora * 1.14
 
-print("Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
+print(
+    "Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
       hora extra domingo e feriados[3], hora extra domingo e feriados noturno[4]\
-      ? [0/1/2/3/4]")
+      ? [0/1/2/3/4]"
+)
 tipo_1 = input("> ")
 print("Trabalhou quantas horas extras? ")
 quantidade_de_horas_extras1 = float(input("> "))
-horas_extra_1 = hora_extra(quantidade_de_horas_extras1, valor_da_hora, 
-                           valor_hora_noturna, tipo_1)
+horas_extra_1 = hora_extra(
+    quantidade_de_horas_extras1, valor_da_hora, valor_hora_noturna, tipo_1
+)
 
-print("Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
+print(
+    "Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
       hora extra domingo e feriados[3], hora extra domingo e feriados noturno[4]\
-      ? [0/1/2/3/4]")
+      ? [0/1/2/3/4]"
+)
 tipo_2 = input("> ")
 print("Trabalhou quantas horas extras? ")
 quantidade_de_horas_extras2 = float(input("> "))
-horas_extra_2 = hora_extra(quantidade_de_horas_extras2, valor_da_hora, 
-                           valor_hora_noturna, tipo_2)
+horas_extra_2 = hora_extra(
+    quantidade_de_horas_extras2, valor_da_hora, valor_hora_noturna, tipo_2
+)
 
-print("Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
+print(
+    "Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
       hora extra domingo e feriados[3], hora extra domingo e feriados noturno[4]\
-      ? [0/1/2/3/4]")
+      ? [0/1/2/3/4]"
+)
 tipo_3 = input("> ")
 print("Trabalhou quantas horas extras? ")
 quantidade_de_horas_extras3 = float(input("> "))
-horas_extra_3 = hora_extra(quantidade_de_horas_extras3, valor_da_hora, 
-                           valor_hora_noturna, tipo_3)
+horas_extra_3 = hora_extra(
+    quantidade_de_horas_extras3, valor_da_hora, valor_hora_noturna, tipo_3
+)
 
-print("Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
+print(
+    "Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
       hora extra domingo e feriados[3], hora extra domingo e feriados noturno[4]\
-      ? [0/1/2/3/4]")
+      ? [0/1/2/3/4]"
+)
 tipo_4 = input("> ")
 print("Trabalhou quantas horas extras? ")
 quantidade_de_horas_extras4 = float(input("> "))
-horas_extra_4 = hora_extra(quantidade_de_horas_extras4, valor_da_hora, 
-                           valor_hora_noturna, tipo_4)
+horas_extra_4 = hora_extra(
+    quantidade_de_horas_extras4, valor_da_hora, valor_hora_noturna, tipo_4
+)
 
-print("Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
+print(
+    "Trabalhou horas extras normais[0], hora extra noturna[1], hora notura[2],\
       hora extra domingo e feriados[3], hora extra domingo e feriados noturno[4]\
-      ? [0/1/2/3/4]")
+      ? [0/1/2/3/4]"
+)
 tipo_5 = input("> ")
 print("Trabalhou quantas horas extras? ")
 quantidade_de_horas_extras5 = float(input("> "))
-horas_extra_5 = hora_extra(quantidade_de_horas_extras5, valor_da_hora, 
-                           valor_hora_noturna,tipo_5)
+horas_extra_5 = hora_extra(
+    quantidade_de_horas_extras5, valor_da_hora, valor_hora_noturna, tipo_5
+)
 
-salario_adicional_mais_hora_extra = salario_adicional + horas_extra_1 + \
-    horas_extra_2 + horas_extra_3 + horas_extra_4 + horas_extra_5
+salario_adicional_mais_hora_extra = (
+    salario_adicional
+    + horas_extra_1
+    + horas_extra_2
+    + horas_extra_3
+    + horas_extra_4
+    + horas_extra_5
+)
 
 print("Quantos dias úteis houve nesse mês: ")
 dias_uteis = int(input("> "))
@@ -223,19 +263,28 @@ valor_falta = faltas(horas_faltadas, salario_adicional, dias_faltados, carga)
 
 domingos_feriados = domingos_feriados - dias_faltados
 
-dsr1, dsr2, dsr3, dsr4, dsr5 = dsr(dias_uteis,domingos_feriados,
-                                   (quantidade_de_horas_extras1, 
-                                    quantidade_de_horas_extras2, 
-                                    quantidade_de_horas_extras3,
-                                    quantidade_de_horas_extras4,
-                                    quantidade_de_horas_extras5),
-                                   (horas_extra_1, horas_extra_2,
-                                    horas_extra_3, horas_extra_4,
-                                    horas_extra_5)
-                                   )
+dsr1, dsr2, dsr3, dsr4, dsr5 = dsr(
+    dias_uteis,
+    domingos_feriados,
+    quantidade_de_horas_extras=(
+        quantidade_de_horas_extras1,
+        quantidade_de_horas_extras2,
+        quantidade_de_horas_extras3,
+        quantidade_de_horas_extras4,
+        quantidade_de_horas_extras5,
+    ),
+    horas_extras=(
+        horas_extra_1,
+        horas_extra_2,
+        horas_extra_3,
+        horas_extra_4,
+        horas_extra_5,
+    ),
+)
 
-total_proventos = salario_adicional_mais_hora_extra + \
-    dsr1 + dsr2 + dsr3 + dsr4 + dsr5
+total_proventos = (
+    salario_adicional_mais_hora_extra + dsr1 + dsr2 + dsr3 + dsr4 + dsr5
+)
 
 total_proventos_menos_faltas = total_proventos - valor_falta
 
@@ -243,7 +292,7 @@ inss = inss(total_proventos_menos_faltas)
 
 total_proventos_menos_faltas_menos_inss = total_proventos_menos_faltas - inss
 
-irrf = irrf(total_proventos_menos_faltas_menos_inss,dependets, pens, previ)
+irrf = irrf(total_proventos_menos_faltas_menos_inss, dependets, pens, previ)
 
 salario_liquido = total_proventos_menos_faltas_menos_inss - irrf
 
@@ -257,7 +306,8 @@ print(f"Valor da hora extra 3: R$ {horas_extra_3:.2f}")
 print(f"Valor da hora extra 4: R$ {horas_extra_4:.2f}")
 print(f"Valor da hora extra 5: R$ {horas_extra_5:.2f}")
 print(
-    f"Total do valor de horas extras: R$ {horas_extra_1 + horas_extra_2 + horas_extra_3 + horas_extra_4 + horas_extra_5: .2f}")
+    f"Total do valor de horas extras: R$ {horas_extra_1 + horas_extra_2 + horas_extra_3 + horas_extra_4 + horas_extra_5: .2f}"
+)
 print(f"DSR 1: R$ {dsr1:.2f}")
 print(f"DSR 2: R$ {dsr2:.2f}")
 print(f"DSR 3: R$ {dsr3:.2f}")
@@ -267,9 +317,11 @@ print(f"Total de DSR: R$ {dsr1 + dsr2 + dsr3 + dsr4 + dsr5:.2f}")
 print(f"Total de proventos: R$ {total_proventos:.2f}")
 print(f"Valor das faltas: R$ {valor_falta:.2f}")
 print(
-    f"Total de proventos menos as faltas: R$ {total_proventos_menos_faltas:.2f}")
+    f"Total de proventos menos as faltas: R$ {total_proventos_menos_faltas:.2f}"
+)
 print(f"Valor do INSS: R$ {inss:.2f}")
 print(
-    f"Total de proventos menos faltas menos INSS: R$ {total_proventos_menos_faltas_menos_inss:.2f}")
+    f"Total de proventos menos faltas menos INSS: R$ {total_proventos_menos_faltas_menos_inss:.2f}"
+)
 print(f"IRFF: R$ {irrf:.2f}")
 print(f"Salário líquido: R$ {salario_liquido:.2f}")
